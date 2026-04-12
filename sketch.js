70c1c7b709a10baeec377e5ae47216b2ef21bfd8
boundary-x <hi@boundaryx.io>
2026-04-12 10:20:58 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -388,7 +388,7 @@ async function predictWebcam() {
 async function connectBluetooth() {
   try {
     bluetoothDevice = await navigator.bluetooth.requestDevice({
-      filters: [{ name: "ESP" }],
+      filters: [{ namePrefix: "ESP" }],
       optionalServices: [UART_SERVICE_UUID]
     });
     const server = await bluetoothDevice.gatt.connect();
